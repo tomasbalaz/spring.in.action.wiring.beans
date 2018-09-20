0ee4a16d80fe48c1e39cd63dc6d3421a267a0620
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-09-20 12:18:30 +0200

    Add possibilities how to set up @ComponentScan

--- src/main/java/spring/in/action/spring/in/action/wiring/beans/config/CDPlayerConfig.java
+++ src/main/java/spring/in/action/spring/in/action/wiring/beans/config/CDPlayerConfig.java
@@ -3,8 +3,28 @@ package spring.in.action.spring.in.action.wiring.beans.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import spring.in.action.spring.in.action.wiring.beans.soundsystem.SgtPepperss;
+
 @Configuration
-@ComponentScan("spring.in.action.spring.in.action.wiring")
+
+//Works only when all classes are in same package
+//@ComponentScan
+
+//Works for specfied package
+//@ComponentScan("spring.in.action.spring.in.action.wiring")
+
+//Works for specfied package with basePackages
+//@ComponentScan(basePackages= "spring.in.action.spring.in.action.wiring")
+
+//Works for specfied packages with basePackages
+//@ComponentScan(basePackages= {"spring.in.action.spring.in.action.wiring.beans", "spring.in.action.spring.in.action.wiring.soundsystem", "spring.in.action.spring.in.action.wiring.config"})
+
+//Works for specfied class
+//@ComponentScan(basePackageClasses= SgtPepperss.class)
+
+//Works for specfied classes
+@ComponentScan(basePackageClasses= {SgtPepperss.class})
+
 public class CDPlayerConfig {
 
 }
