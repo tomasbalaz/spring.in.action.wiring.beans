71eb07cf0fcb05e96f65f373eeaca27a0f33b746
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-09-20 08:40:15 +0200

    Initialize commit

---
+++ src/main/java/spring/in/action/spring/in/action/wiring/beans/App.java
@@ -0,0 +1,13 @@
+package spring.in.action.spring.in.action.wiring.beans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
